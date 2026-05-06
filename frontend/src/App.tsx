c948967e82c811e54f40d14e9bf91f32704a0de7
Ali <alytaha098@gmail.com>
2026-05-06 16:38:19 +0300

    feat: init react , vite , tailwindcss ,shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <div >
+      <h1>Welcome to React</h1>
+      <Button>Click Me</Button>
+      </div>
+  )
+}
+
+export default App
